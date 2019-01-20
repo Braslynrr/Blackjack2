9a71ccbacff384b08e57893086d33ec303c4a794
MARIO ARGUELLO BORGE <mariozdz@gmail.com>
2019-01-20 16:07:16 -0600

    " base para guardar juego en juego.h"

--- Blackjack/Juego.cpp
+++ Blackjack/Juego.cpp
@@ -7,3 +7,20 @@ juego::~juego()
 	delete listaJugadores;
 	delete baraja;
 }
+
+void juego::guardarPartida(std::string nombre)
+{
+
+
+	std::ofstream handle; 
+
+	handle.open(nombre  += ".txt", std::ios::out); 
+	if (handle.fail())  
+	{
+		std::cout << "Error al crear\n";
+		exit(1);
+	}
+	
+		
+	handle.close();
+}
